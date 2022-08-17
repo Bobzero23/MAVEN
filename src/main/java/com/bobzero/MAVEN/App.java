47339d0ca60016587c69314d35af9786c7992421
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-17 11:16:31 +0300

    WORKING WITH MAVEN

--- src/main/java/com/bobzero/MAVEN/App.java
+++ src/main/java/com/bobzero/MAVEN/App.java
@@ -1,14 +1,13 @@
 package com.bobzero.MAVEN;
 
-/**
- * 
+/*
  * Hello world!
- *
  */
-public class App 
+
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
 }
